810b41150d7b31b5e3e7056ed7227a05c0659978
Rehtang <Warrior8584!>
2022-09-23 12:45:57 +0300

    completed rest

--- src/main/kotlin/ru/rehtang/rest/CryptocurrencyRestController.kt
+++ src/main/kotlin/ru/rehtang/rest/CryptocurrencyRestController.kt
@@ -1,4 +1,29 @@
 package ru.rehtang.rest
 
-class CryptocurrencyRestController {
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import ru.rehtang.dto.BybitDto
+import ru.rehtang.dto.HuobiDto
+import ru.rehtang.dto.MessariDto
+import ru.rehtang.service.CryptocurrencyProvideService
+
+@RestController
+class CryptocurrencyRestController(private val service: CryptocurrencyProvideService) {
+  @GetMapping("/bybit")
+  fun getBybitMarket(
+      @RequestParam symbol: String,
+      @RequestParam interval: String,
+      @RequestParam limit: Int
+  ): BybitDto {
+    return service.receiveBybitMarket(symbol, interval, limit)
+  }
+  @GetMapping("/messari")
+  fun getMessariMarket(@RequestParam symbol: String): MessariDto {
+    return service.receiveMessariMarket(symbol)
+  }
+  @GetMapping("/huobi")
+  fun getHuobiMarket(@RequestParam symbol: String): HuobiDto {
+    return service.receiveHuobiMarket(symbol)
+  }
 }
